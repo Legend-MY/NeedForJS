8e86bc08b92ceca17caa07829a3a798be5701bda
Legend-MY <markov2008@bigmir.net>
2020-09-08 17:25:20 +0300

    Добавил некоторые стили + задал переменные и функции

--- main.js
+++ main.js
@@ -1,3 +1,22 @@
 const score = document.querySelector('.score'),
   start = document.querySelector('.start'),
   gameArea = document.querySelector('.gameArea');
+
+
+start.addEventListener('click', startGame);
+document.addEventListener('keydown', startRun);
+document.addEventListener('keyup', stopRun);
+
+
+function startGame() {
+  start.classList.add('hide');
+}
+
+function startRun(event) {
+  event.preventDefault();
+  console.log(event);
+}
+
+function stopRun(event) {
+  console.log('stop');
+}
